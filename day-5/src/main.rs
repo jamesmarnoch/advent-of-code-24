a0a1e9f0314ff38d597e92f40b874804355b6ee4
jamesmarnoch <james.marnoch@gmail.com>
2024-12-08 13:04:49 +0000

    day 5

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -2,7 +2,27 @@ use std::fs;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let buf_reader = BufReader::new(fs::File::open("input.txt").unwrap());
+    let buf_reader = BufReader::new(fs::File::open("input2.txt").unwrap());
 
     let lines = buf_reader.lines();
+    let mut rules = Vec::<String>::new();
+    let mut updates = Vec::<String>::new();
+    let mut is_rules = true;
+    lines.for_each(|line|
+        {
+            let l = line.unwrap();
+            if is_rules {
+                if l.is_empty()
+                {
+                    is_rules = false;
+                } else {
+                    rules.push(l);
+                }
+            } else {
+                updates.push(l);
+            }
+        });
+
+    println!("Rules: {:?}", rules);
+    println!("Updates: {:?}", updates);
 }
